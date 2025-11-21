ec56328f6079980f65980b1399837178ac183b1e
a41216474@gmail.com <Azamat>
2025-11-21 06:51:55 +0500

    asd

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,15 +1,4 @@
-// import React from "react";
-// import { Navigate } from "react-router-dom";
 
-// export default function PrivateRoute({ children }) {
-//   const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
-
-//   if (!isLoggedIn) {
-//     return <Navigate to="/login" replace />;
-//   }
-
-//   return children;
-// }
 import React from "react";
 import { Navigate } from "react-router-dom";
 
